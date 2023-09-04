fcb226424e0b8e14fb46ca92d16a88c9294192ef
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-09-04 19:17:55 +0530

    added dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/ui/Button'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <Button size="sm" />
+  )
+}
+
+export default DashboardPage
